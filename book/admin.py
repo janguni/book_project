9dd9436118a9c61d1ed21aa76b6d5aefa15c07bc
janguni <lightson23@naver.com>
2022-05-31 16:44:23 +0900

    위시리스트 기본 틀 생성

--- book/admin.py
+++ book/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from import_export.admin import ImportExportMixin
-from .models import User, Book
+from .models import User, Book,WishBookList
 # Register your models here.
 
 class BookAdmin(ImportExportMixin, admin.ModelAdmin):
@@ -10,3 +10,4 @@ class BookAdmin(ImportExportMixin, admin.ModelAdmin):
 UserAdmin.fieldsets += (("Custom fields",{"fields":("nickname","profile_pic","intro")}),)
 admin.site.register(User,UserAdmin)
 admin.site.register(Book,BookAdmin)
+admin.site.register(WishBookList)
--- book/models.py
+++ book/models.py
@@ -35,6 +35,21 @@ class Book(models.Model):
     book_publisher = models.CharField(max_length=100)
     genre_name = models.CharField(max_length=50)
 
+
     class Meta:
         db_table = 'bookList.csv'
+
+    def get_absolute_url(self):
+        return f'/book/{self.book_isbn}/'
+
+class WishBookList(models.Model):
+    user_id= models.ForeignKey(
+        "User", related_name="wish_user", on_delete=models.CASCADE, null=True)
+    book_id= models.ForeignKey(
+        "Book", related_name="wish_book", on_delete=models.CASCADE, null=True)
+    #count = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f'[{self.pk}] {self.user_id} like {self.book_id}'
+    
     
